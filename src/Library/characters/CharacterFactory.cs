7b802ef84309e6913b8f8626865846b9f1632e43
Gonzalo <87908978+Gonza50@users.noreply.github.com>
2022-10-31 11:06:57 -0300

    Escenario Listo

--- src/Library/characters/CharacterFactory.cs
+++ src/Library/characters/CharacterFactory.cs
@@ -6,7 +6,9 @@ namespace RoleplayGame.Characters
     public enum CharacterType
     {
         Elf = 1,
-        Wizard = 2
+        Wizard = 2,
+        Troll = 3,
+        Princesa = 4,
     }
 
     /// <summary>
@@ -23,6 +25,8 @@ namespace RoleplayGame.Characters
             {
                 case CharacterType.Elf: return new Elf(name);
                 case CharacterType.Wizard: return new Wizard(name);
+                case CharacterType.Troll: return new Troll(name);
+                case CharacterType.Princesa: return new Princesa(name);
 
                 default: throw new System.Exception($"Invalid character {type}");
             }
